c98edab0759aad397fd83afd0254b4f6810c808a
Gabriel Horner <gabriel.horner@gmail.com>
2012-05-20 21:37:29 -0400

    add #spy for debugging

--- user.clj
+++ user.clj
@@ -2,8 +2,12 @@
   (:require  [clojure.pprint :as pp])
 )
 
-(defn load-path []
+(defn class-path "Prints list of class paths" []
   (pp/pprint  (seq  (.getURLs  (java.lang.ClassLoader/getSystemClassLoader))))
 )
+
+(defn spy "Simple print debugging" [& args]
+  (do (println (flatten args))
+      (flatten (identity args))))
 
 (println "Loaded user.clj!")
