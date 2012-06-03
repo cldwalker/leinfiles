8db140b55c1d2040e544099b41249ff3d0ec9389
Gabriel Horner <gabriel.horner@gmail.com>
2012-06-03 17:47:59 -0400

    add java-methods, wip ls

--- user.clj
+++ user.clj
@@ -10,4 +10,16 @@
   (do (println (flatten args))
       (flatten (identity args))))
 
+(defn java-methods "List of methods for a java class" [klass]
+  (sort (distinct (map #(.getName %) (seq (.getMethods klass))))))
+
+; TODO: can't construct full symbol for doc correctly
+;(defmacro ls
+;  "Prints docs for all vars in a namespace"
+;  [nsname]
+;  `(doseq [v# (clojure.repl/dir-fn '~nsname)]
+;     (clojure.repl/doc
+;     (symbol (str '~nsname "/" v#))))
+;  )
+
 (println "Loaded user.clj!")
