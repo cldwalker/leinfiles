aeece86e277c76b0616c657d424432fa6f4b8a88
Gabriel Horner <gabriel.horner@gmail.com>
2012-06-29 20:09:47 -0400

    add doc-dir and dir-mtable, ns-interns ftw

--- user.clj
+++ user.clj
@@ -1,5 +1,6 @@
 (ns u
   (:require [clojure.pprint :as pp])
+  (:require [clojure.string :as string])
 )
 
 (defn class-path "Prints list of class paths" []
@@ -17,14 +18,24 @@
 (defn mtable "Prints meta of a function as a table" [sym]
   (table.core/table (meta (resolve sym))))
 
-; TODO: can't construct full symbol for doc correctly
-;(defmacro ls
-;  "Prints docs for all vars in a namespace"
-;  [nsname]
-;  `(doseq [v# (clojure.repl/dir-fn '~nsname)]
-;     (clojure.repl/doc
-;     (symbol (str '~nsname "/" v#))))
-;  )
+(defn dir-mtable [nsname]
+  (table.core/table (map #( meta (resolve %)) (clojure.repl/dir-fn nsname))))
+
+(defn sym-to-var [sym]
+  "Converts a symbol to var"
+  ((ns-interns ((meta (resolve sym)) :ns)) sym))
+
+(defn split [f coll]
+  "Like ruby's partition"
+  [(filter f coll) (remove f coll)])
+
+(defn doc-dir [nsname]
+  "Prints docs for a given namespace"
+  (let [ [resolved unresolved] (split resolve (clojure.repl/dir-fn nsname)) ]
+    (doseq [sym resolved]
+      (@#'clojure.repl/print-doc (meta (sym-to-var sym))))
+    (when-not (empty? unresolved)
+      (println (str "\n" "Unable to resolve these symbols: " (string/join ", " unresolved))))))
 
 (set! clojure.core/*print-length* 100)
 (set! clojure.core/*print-level* 5)
