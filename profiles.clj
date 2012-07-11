89e701ab0987fac4ad8dd662b0bf51b7ec532380
Gabriel Horner <gabriel.horner@gmail.com>
2012-07-10 20:11:40 -0400

    add lein-light

--- profiles.clj
+++ profiles.clj
@@ -4,5 +4,5 @@
      (use '[table.core :only (table)])
      (use '[clojure.repl :only (dir-fn)])
      (load-file (str (System/getProperty "user.home") "/.lein/user.clj")))}
-  :plugins  [[lein-clojars "0.9.0"]]
+  :plugins  [[lein-clojars "0.9.0"] [lein-light "0.0.4"]]
   :dependencies  {table "0.2.0"}}}
