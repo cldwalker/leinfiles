ffc453ea76daa33fba0143d54c945cf016710abb
Gabriel Horner <gabriel.horner@gmail.com>
2012-07-25 00:51:53 -0400

    fix profiles - was causing lein pom to generate false dependencies

--- profiles.clj
+++ profiles.clj
@@ -4,5 +4,7 @@
      (use '[table.core :only (table)])
      (use '[clojure.repl :only (dir-fn)])
      (load-file (str (System/getProperty "user.home") "/.lein/user.clj")))}
-  :plugins  [[lein-clojars "0.9.0"] [lein-light "0.0.4"]]
-  :dependencies  {table "0.2.0" com.datomic/datomic "0.1.3164" }}}
+    :plugins  [[lein-clojars "0.9.0"] [lein-light "0.0.4"]]
+  }
+  :dependencies  {table "0.2.0" com.datomic/datomic "0.1.3164" }
+}
