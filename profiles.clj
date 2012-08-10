53dbb2ec57897941cb4935f912c1479f1bafb8a5
Gabriel Horner <gabriel.horner@gmail.com>
2012-08-10 15:52:33 -0400

    reformat profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,10 +1,11 @@
-{:user
- {:repl-options
-   {:init (do
-     (use '[table.core :only (table)])
-     (use '[clojure.repl :only (dir-fn)])
-     (load-file (str (System/getProperty "user.home") "/.lein/user.clj")))}
-    :plugins  [[lein-clojars "0.9.0"] [lein-light "0.0.4"]]
-  }
-  :dependencies  {table "0.2.0" com.datomic/datomic "0.1.3164" }
+{
+ :user {
+   :plugins [[lein-clojars "0.9.0"] [lein-light "0.0.4"]]
+   :repl-options {
+     :init (do
+       (use '[table.core :only (table)])
+       (use '[clojure.repl :only (dir-fn)])
+       (load-file (str (System/getProperty "user.home") "/.lein/user.clj"))) }
+ }
+ :dependencies {table "0.3.2" com.datomic/datomic "0.1.3164"}
 }
