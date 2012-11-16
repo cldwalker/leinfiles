43b96414776fa633e9c3bbdc18a96c16876a6420
Gabriel Horner <gabriel.horner@gmail.com>
2012-11-16 15:33:04 -0500

    umm, local update would be nice

--- profiles.clj
+++ profiles.clj
@@ -9,6 +9,6 @@
        (use '[clojure.repl :only (dir-fn)])
        (load-file (str (System/getProperty "user.home") "/.lein/user.clj"))) }
    :dependencies [[table "0.3.2"] [desc "0.1.0"]
-                  [rubydoc "0.2.0"]]
+                  [rubydoc "0.3.0"]]
  }
 }
