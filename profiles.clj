62ee8b36b9ab55cfd15f8c050b9a1eb4953346c2
Gabriel Horner <gabriel.horner@gmail.com>
2013-02-12 00:50:18 -0500

    deps-tree is native, try exec

--- profiles.clj
+++ profiles.clj
@@ -3,11 +3,11 @@
    :plugins [
              ;[lein-clojars "0.9.0"]
              ;[lein-light "0.0.4"]
-             [lein-deps-tree "0.1.2"]
              [lein-difftest "2.0.0"]
              [lein-grep "0.1.0"]
              [lein-open "0.1.0"]
              [lein-outdated "1.0.0"]
+             [lein-exec "0.3.0"]
              ]
    :repl-options {
      :init (do
